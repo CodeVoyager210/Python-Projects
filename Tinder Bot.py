7a6a40fd98994ce7b2d5ed814778948b8da27b7b
Efthimis <athanasopoulos.efth@gmail.com>
2024-09-24 17:25:24 +0300

    Fixed the issue using Facebook instead of Google

--- Tinder Bot.py
+++ Tinder Bot.py
@@ -1,35 +1,83 @@
-import time
+from time import sleep
+
+from numpy.ma.core import inner
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException,ElementClickInterceptedException,StaleElementReferenceException
+while True:
+    inner_loop = False
+    WEBSITE = 'https://tinder.com'
+    EMAIL = 'wanderercosmic69@gmail.com'
+    PASSWORD = 'November32@'
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option('detach', True)
+    driver = webdriver.Chrome(options=chrome_options)
+    driver.get(WEBSITE)
+    sleep(2)
+    login = driver.find_element(By.CSS_SELECTOR, value="a[href='https://tinder.onelink.me/9K8a/3d4abb81']")
+    sleep(2)
+    login.click()
+    sleep(2)
+    fb = driver.find_element(By.XPATH,
+                             value='//*[@id="o672545042"]/div/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button')
+    sleep(2)
+    fb.click()
+    sleep(2)
+    fb_window = driver.window_handles[1]
+    driver.switch_to.window(fb_window)
+    sleep(2)
+    cookies = driver.find_element(By.XPATH,
+                                  value='//*[@id="facebook"]/body/div[2]/div[2]/div/div/div/div/div[3]/div[2]/div/div[2]/div[1]')
+    sleep(2)
+    cookies.click()
+    sleep(2)
+    email = driver.find_element(By.ID, value='email')
+    email.send_keys(EMAIL)
+    password = driver.find_element(By.ID, value='pass')
+    password.send_keys(PASSWORD)
+    fb_login = driver.find_element(By.ID, value='loginbutton')
+    fb_login.click()
+    sleep(12)
+    cont = driver.find_element(By.CSS_SELECTOR, value='div[role="button"]')
+    cont.click()
+    main_window = driver.window_handles[0]
+    driver.switch_to.window(main_window)
+    sleep(6)
+    try:
+        location = driver.find_element(By.XPATH, value='//*[@id="o672545042"]/div/div[1]/div/div/div[3]/button[1]')
+        location.click()
+        sleep(1)
+        noty = driver.find_element(By.XPATH, value='//*[@id="o672545042"]/div/div[1]/div/div/div[3]/button[2]')
+        noty.click()
+        sleep(12)
+        decline = driver.find_element(By.XPATH,
+                                      value='//*[@id="o-1894041178"]/div/div[1]/div/main/div[1]/div/div/div/div[1]/div[1]/div/div[3]/div/div[2]')
+        while True:
+            sleep(1)
+            try:
+                decline.click()
+            except ElementClickInterceptedException:
+                sleep(1)
+                home_screen = driver.find_element(By.XPATH,value='//*[@id="o672545042"]/div/div[1]/div[2]/button[2]')
+                home_screen.click()
+            except StaleElementReferenceException:
+                driver.quit()
+                inner_loop = True
+                print('You have run out of matches in your area,Check again later')
+                break
+
+    except NoSuchElementException:
+        print('Retrying')
+        driver.quit()
+    if inner_loop :
+        break
+input('Press any key to close the program')
 
 
-WEBSITE = 'https://tinder.com'
-EMAIL = 'fabulousontesting@gmail.com'
-PASSWORD = 'November_32@'
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option('detach',True)
-chrome_options.add_argument("--disable-extensions")
-chrome_options.add_argument("--disable-popup-blocking")
-chrome_options.add_argument("--profile-directory=Default")
-chrome_options.add_argument("--disable-plugins-discovery")
-chrome_options.add_argument("--incognito")
-chrome_options.add_argument("user_agent=DN")
-driver = webdriver.Chrome(options=chrome_options)
-driver.get(WEBSITE)
-time.sleep(2)
-login = driver.find_element(By.CSS_SELECTOR,value="a[href='https://tinder.onelink.me/9K8a/3d4abb81']")
-time.sleep(2)
-login.click()
-time.sleep(2)
-google = driver.find_element(By.CSS_SELECTOR,value='iframe')
-time.sleep(2)
-google.click()
-time.sleep(2)
-google_window = driver.window_handles[1]
-driver.switch_to.window(google_window)
-time.sleep(2)
-email = driver.find_element(By.CSS_SELECTOR,value='input')
-email.send_keys(EMAIL)
+
+
+
+
 
 
 
